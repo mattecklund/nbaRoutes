97027f928b6297c3f10c5f4a88112222fe7e8fa3
Matthew Ecklund <mecklund@ldschurch.org>
2015-05-13 18:09:26 -0600

    finished with step 5

--- js/teams/teamCtrl.js
+++ js/teams/teamCtrl.js
@@ -1,5 +1,48 @@
 var app = angular.module('nbaRoutes');
 
-app.controller('teamCtrl', function($scope, $routeParams, teamService){
+app.controller('teamCtrl', function($scope, $routeParams, teamService, teamData){
+	$scope.teamData = teamData;
+	$scope.newGame = {};
+	$scope.showNewGameForm = false;
 
+	// console.log(teamData);
+	console.log($scope);
+
+	$scope.teams = ["Boston Celtics", "Brooklyn Nets", "New York Knicks", "Philadelphia 76ers", "Toronto Raptors", "Chicago Bulls", "Cleveland Cavaliers", "Detroit Pistons", "Indiana Pacers", "Milwaukee Bucks", "Atlanta Hawks", "Charlotte Hornets", "Miami Heat", "Orlando Magic", "Washington Wizards", "Dallas Mavericks", "Houston Rockets", "Memphis Grizzlies", "New Orleans Pelicans", "San Antonio Spurs", "Denver Nuggets", "Minnesota Timberwolves", "Portland Trail Blazers", "Oklahoma City Thunder", "Utah Jazz", "Golden State Warriors", "Los Angeles Clippers", "Los Angeles Lakers", "Phoenix Suns", "Sacramento Kings"];
+
+	$scope.toggleNewGameForm = function(){
+		$scope.showNewGameForm = !$scope.showNewGameForm;
+		console.log($scope.showNewGameForm);
+	}
+
+	if($routeParams.team === 'utahjazz'){
+		$scope.homeTeam = 'Utah Jazz';
+		$scope.logoPath = 'images/jazz-logo.png'
+	} else if($routeParams.team === 'losangeleslakers'){
+		$scope.homeTeam = 'Los Angeles Lakers';
+		$scope.logoPath = 'images/lakers-logo.png';
+	} else if($routeParams.team === 'miamiheat'){
+		$scope.homeTeam = 'Miami Heat';
+		$scope.logoPath = 'images/heat-logo.png';
+	} else {
+		$scope.homeTeam = 'Unknown';
+	}
+
+	$scope.submitGame = function(){
+		$scope.newGame.homeTeam = $scope.homeTeam.split(' ').join('').toLowerCase();
+		teamService.addNewGame($scope.newGame).then(
+			function(){
+				teamService.getTeamData($scope.newGame.homeTeam).then(
+					function(response){
+						$scope.teamData = response;
+						$scope.newGame = {};
+						$scope.showNewGameForm = false;
+					},function(err){
+						console.log('We made another boo-boo');
+					}
+				)
+			},function(error){
+				console.log('We made a boo-boo');
+			});
+	}
 });
--- js/teams/teamService.js
+++ js/teams/teamService.js
@@ -1,5 +1,47 @@
 var app = angular.module('nbaRoutes');
 
 app.service('teamService', function($http, $q){
+	this.addNewGame = function(gameObj){
+		//sned to parse I think
+		var parseUrl = 'https://api.parse.com/1/classes/' + gameObj.homeTeam;
+		if (parseInt(gameObj.homeTeamScore) > parseInt(gameObj.opponentScore)){
+			gameObj.won = true
+		} else {
+			gameObj.won = false;
+		};
+		console.log(gameObj);
+		return $http({
+			method:'POST',
+			url: parseUrl,
+			data: gameObj
+		})
+	};
 
+	this.getTeamData = function(team){
+		var deferred = $q.defer();
+		var url = 'https://api.parse.com/1/classes/' + team;
+		$http({
+			method: 'GET',
+			url: url
+		}).then(function(data){
+			var results = data.data.results;
+			// console.log(results);
+			var wins = 0;
+			var losses = 0;
+			for (var i = 0; i < results.length; i++){
+				if(results[i].won === true){
+					wins++;
+				} else {
+					losses++;
+				}
+			};
+			results['wins'] = wins;
+			results['losses'] = losses;
+			// console.log(results);
+			deferred.resolve(results);
+		}, function(error){
+			deferred.reject;
+		})
+		return deferred.promise;
+	}
 });
